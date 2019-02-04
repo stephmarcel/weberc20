fa2db1734f20c9b70961c807e567594ef847c4d2
Stéphane-Marcel KOUAME <stephmarcel.kouame@gmail.com>
2019-02-04 10:04:42 +0000

    V1 Election script

---
+++ contracts/Election.sol
@@ -0,0 +1,46 @@
+pragma solidity >=0.4.24 <0.6.0;
+
+contract Election {
+
+
+  struct Candidate{
+    uint id;
+    string name;
+    uint voteCount;
+  }
+  //string candidate;
+
+  event votedEvent(uint indexed _candidateId);
+
+ mapping (uint => Candidate) public candidates;
+ mapping (address => bool) public voters;
+
+ uint public candidatesCount;
+
+  constructor() public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string memory _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted votedEvent
+      emit  votedEvent(_candidateId);
+    }
+}
